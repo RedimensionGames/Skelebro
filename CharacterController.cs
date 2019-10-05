f1c597b1de5c6ef0ddc3bff5d1f1ddd00f37b91c
RedimensionGames <redimension.games@gmail.com>
2019-10-05 15:21:46 +0530

    Initial Commit

---
+++ CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+}
